5b62913ccc09a1869286f253d7fba77d854a4904
simasbakus <simas.bakus@gmail.com>
2020-11-17 11:51:10 +0200

    Refactoring to fill datagrid

--- ScheduleModifierV2WPF/Employee.cs
+++ ScheduleModifierV2WPF/Employee.cs
@@ -7,12 +7,18 @@ namespace ScheduleModifierV2WPF
     class Employee
     {
         public string Name { get; set; }
-        public List<string> Schedule { get; set; }
+        public List<Week> Schedule { get; set; }
 
         public Employee(string name, List<string> schedule)
         {
             this.Name = name;
-            this.Schedule = schedule;
+
+            //Needs to be adapted not fill first and last Week classes completely!!!!
+            this.Schedule = new List<Week>();
+            for (int i = 0; i < schedule.Count; i+=7)
+            {
+                this.Schedule.Add(new Week(schedule.GetRange(i, 7)));
+            }
         }
     }
 }
---
+++ ScheduleModifierV2WPF/Week.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScheduleModifierV2WPF
+{
+    class Week
+    {
+        public string Monday { get; set; }
+        public string Tuesday { get; set; }
+        public string Wednesday { get; set; }
+        public string Thursday { get; set; }
+        public string Friday { get; set; }
+        public string Saturday { get; set; }
+        public string Sunday { get; set; }
+
+        public Week(List<string> weekdays)
+        {
+            this.Monday = weekdays[0];
+            this.Tuesday = weekdays[1];
+            this.Wednesday = weekdays[2];
+            this.Thursday = weekdays[3];
+            this.Friday = weekdays[4];
+            this.Saturday = weekdays[5];
+            this.Sunday = weekdays[6];
+        }
+    }
+}
